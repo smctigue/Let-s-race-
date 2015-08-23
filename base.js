d0ffbf23ead0b5aca4738c0923255d362a5f2716
smctigue <stephenmctigue@gmail.com>
2015-08-23 15:35:24 -0700

    render characters

--- base.js
+++ base.js
@@ -1,95 +1,92 @@
 console.log('JS is working');
-$(document).ready(function(){
+$(document).ready(function() {
 
+    // Renders gameboard & sprites to page
+    $(function render() {
 
+        var boardTemplate = _.template($('#game-template').html());
 
-	// ---------------------------------------------------
-	// var sonic = document.getElementById('sonicguy');
-	// var sonicontain = document.getElementById('sonicontainer');
+        var racers = players.sprites;
 
-	// var knuckles = document.getElementById('knucklesguy');
-	// var knucklescontain = document.getElementById('knuckcontainer');
-	// ---------------------------------------------------
+        players.sprites.forEach(function(sprites) {
 
+            var boardHtml = boardTemplate(sprites);
+            $('#board-placeholder').append(boardHtml);
+        });
 
+    });
 
+    // Counter - to begin race
+    $('#startbtn').click(function() {
+        var counter = 3;
+        setInterval(function() {
+            counter -= 1;
+            if (counter >= 0) {
+                span = document.getElementById("count");
+                span.innerHTML = counter;
+            }
+            if (counter === 0) {
+                $("#count").text("Go!")
+                clearInterval(counter);
 
-
-	// Get ready to race!
-
-	var moveLeftS = 0;
-	var moveLeftK = 0;
-
-	// Start button - countdown to start race 
-
-	$('#startbtn').click(function(){
-	  var counter = 3;
-	  setInterval(function() {
-	      counter -= 1;
-	    if (counter >= 0) {
-	      span = document.getElementById("count");
-	      span.innerHTML = counter;
-	    }
-	    if (counter === 0) {
-	    	$("#count").text( "Go!" )
-	        clearInterval(counter);
-
-	        //Calls function animate()
-	        document.onkeydown = animate;
-	    }
-	  }, 1000);
-	});
-
-
-	// Move characters 10px per specific keypress / stops them / adds winner to page 
-
-	function animate(x) { 
-		if (x.keyCode === 65) {
-			moveLeftS += 10;
-			sonicguy.style.left = moveLeftS + 'px';
-		}
-		else if (x.keyCode === 76) {
-			moveLeftK += 10;
-			knucklesguy.style.left = moveLeftK + 'px';
-		}
-		winsRace();
-	}
-
-
-	function winsRace(playerName, counter) {
-			var counter = 1100;
-	//TODO:  Turn these two top win conditions into a separate function 
-	// params: playerName counters
-	if (moveLeftS === counter) {
-		winner = 'Sonic';
-			$('.newwinner').text(winner + '!');
-	}
-	else if (moveLeftK === counter) {
-		winner = 'Knuckles';
-			$('.newwinner').text(winner + '!');
-	}
-	//^^^^^^^^^^up here
-	}
-
-
-	// ---------------------------------------------------
-
-  // Closes (document).ready
+                //Calls function animate()
+                document.onkeydown = animate;
+            }
+        }, 1000);
+    });
 });
 
 
 
-var players = {
-  sprite: [
-    {
-      id: 0,
-      image: "imgs/sonicsprite.png",
-      name: "Sonic",
-    },
-    {
-      id: 1,
-      image: "imgs/knucklessprite.png",
-      name: "Knuckles",
+// --------------------Probably unnecessary---------------------
+var sonic = document.getElementById('sonicguy');
+var sonicontain = document.getElementById('sonicontainer');
+
+var knuckles = document.getElementById('knucklesguy');
+var knucklescontain = document.getElementById('knuckcontainer');
+// -------------------------------------------------------------
+
+
+function animate(e) {
+
+    var keyCode = window.keyCode;
+
+    if (keyCode === 65) {
+        sonic += 10;
+        sonic.style.left = sonic + 'px';
+    } else if (keyCode === 76) {
+        sonic += 10;
+        knuckles.style.left = knuckles + 'px';
     }
-  ]
+    winsRace();
+}
+
+
+function winsRace(playerName, counter) {
+
+    var counter = 1100;
+
+    if (sonic === counter) {
+        winner = 'Sonic';
+        $('.newwinner').text(winner + '!');
+    } else if (knuckles === counter) {
+        winner = 'Knuckles';
+        $('.newwinner').text(winner + '!');
+    }
+};
+
+
+
+
+// Data to be rendered
+var players = {
+    sprites: [{
+        id: 0,
+        image: "css/imgs/sonicsprite.png",
+        name: "sonic",
+    }, {
+        id: 1,
+        image: "css/imgs/knucklessprite.png",
+        name: "knuckles",
+    }]
 };
