446cae07a46fe73d3c5b0e067178e098e05f7072
smctigue <stephenmctigue@gmail.com>
2015-08-24 09:23:35 -0700

    made ish work/ish

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Let's race!
 WDI / Week 1 Project
 
-- Two players test their button mashing skills with the speed of Sonic and Knuckles!
+Two players test their button mashing skills with the speed of Sonic and Knuckles:
+  - Click start to begin race
+  - For Sonic (player 1 / blue) press 'a'
+  - For Knuckles (player 2 / red) press 'l'
+  - First player to cross the finish line wins!
--- base.js
+++ base.js
@@ -1,22 +1,7 @@
 console.log('JS is working');
+
 $(document).ready(function() {
 
-    // Renders gameboard & sprites to page
-    $(function render() {
-
-        var boardTemplate = _.template($('#game-template').html());
-
-        var racers = players.sprites;
-
-        players.sprites.forEach(function(sprites) {
-
-            var boardHtml = boardTemplate(sprites);
-            $('#board-placeholder').append(boardHtml);
-        });
-
-    });
-
-    // Counter - to begin race
     $('#startbtn').click(function() {
         var counter = 3;
         setInterval(function() {
@@ -34,59 +19,46 @@ $(document).ready(function() {
             }
         }, 1000);
     });
+
 });
 
-
-
-// --------------------Probably unnecessary---------------------
-var sonic = document.getElementById('sonicguy');
-var sonicontain = document.getElementById('sonicontainer');
-
-var knuckles = document.getElementById('knucklesguy');
-var knucklescontain = document.getElementById('knuckcontainer');
-// -------------------------------------------------------------
-
+/* Move characters 10px per specific keypress */
+var moveSonic = 0;
+var moveKnuckles = 0;
 
 function animate(e) {
-
-    var keyCode = window.keyCode;
-
-    if (keyCode === 65) {
-        sonic += 10;
-        sonic.style.left = sonic + 'px';
-    } else if (keyCode === 76) {
-        sonic += 10;
-        knuckles.style.left = knuckles + 'px';
+    if (e.keyCode === 65 && moveSonic <= 880) {
+        moveSonic += 10;
+        sonicguy.style.left = moveSonic + 'px';
+    } else if (e.keyCode === 76 && moveKnuckles <= 880) {
+        moveKnuckles += 10;
+        knucklesguy.style.left = moveKnuckles + 'px';
     }
-    winsRace();
-}
+    getWinner();
+};
 
 
-function winsRace(playerName, counter) {
-
-    var counter = 1100;
-
-    if (sonic === counter) {
-        winner = 'Sonic';
-        $('.newwinner').text(winner + '!');
-    } else if (knuckles === counter) {
-        winner = 'Knuckles';
-        $('.newwinner').text(winner + '!');
-    }
+function getWinner() {
+    if (moveSonic === 880) {
+        $("#player1").html(function() {
+            $(".newwinner").text(player1.value + '!');
+        });
+    } else if (moveKnuckles === 880) {
+        $("#player1").html(function() {
+            $(".newwinner").text(player2.value + '!');
+        });
+    };
 };
 
 
 
 
-// Data to be rendered
-var players = {
-    sprites: [{
-        id: 0,
-        image: "css/imgs/sonicsprite.png",
-        name: "sonic",
-    }, {
-        id: 1,
-        image: "css/imgs/knucklessprite.png",
-        name: "knuckles",
-    }]
-};
+// --------------------------------------------------
+// var sonic = document.getElementById('sonicguy');
+// var sonicontain = document.getElementById('sonicontainer');
+
+// var knuckles = document.getElementById('knucklesguy');
+// var knucklescontain = document.getElementById('knuckcontainer');
+// ---------------------------------------------------
+
+// ---------------------------------------------------
